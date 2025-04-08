a0cd7b3ec74302b4d6f175fc7e944112e762dcfe
Irfan Rangga <irfanrangga04@gmail.com>
2025-04-08 07:26:09 +0700

    menambahkan class DataMahasiswa_103022300100

---
+++ JurnalMod7/DataMahasiswa_103022300100.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace JurnalMod7
+{
+    public class Address
+    {
+        public string streetAddress { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+    }
+
+    public class Courses
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    class DataMahasiswa_103022300100
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public Address address { get; set; }
+        public List<Courses> courses { get; set; }
+    }
+
+    public class JsonMahasiswa
+    {
+        public static T ReadJson<T>(string filePath)
+        {
+            string jsonFile = File.ReadAllText(filePath);
+            T result = JsonSerializer.Deserialize<T>(jsonFile);
+
+            return result;
+        }
+    }
+
+
+}
---
+++ JurnalMod7/MainClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JurnalMod7
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            DataMahasiswa_103022300100 mahasiswa = JsonMahasiswa.ReadJson<DataMahasiswa_103022300100>("jurnal7_1_103022300100.json");
+
+            Console.WriteLine($"Nama: {mahasiswa.firstName} {mahasiswa.lastName}");
+            Console.WriteLine($"Gender: {mahasiswa.gender}");
+            Console.WriteLine($"Age: {mahasiswa.age}");
+            Console.WriteLine($"Address: {mahasiswa.address.streetAddress} {mahasiswa.address.city} {mahasiswa.address.state}");
+            Console.WriteLine($"Courses: ");
+            int i = 1;
+            foreach (var matkul in mahasiswa.courses)
+            {
+                Console.WriteLine($"MK {i} {matkul.code} - {matkul.name}");
+                i++;
+            }
+
+        }
+    }
+}
